af07ec3c38f82fcf0511c0321b1990da2ed816b2
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-08 21:21:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,19 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition#, :brand
+  attr_accessor :color, :size, :material, :condition, :brand
 
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand
   end
 
-  def brand=(brand)
-    @brand = brand
-    if Shoe::BRANDS.detect {|brand| brand == @brand} == false
-      BRANDS << @brand
-    end
-  end
+#  def brand=(brand)
+#    @brand = brand
+#    if Shoe::BRANDS.detect {|brand| brand == @brand} == false
+#      BRANDS << @brand
+#    end
+#  end
 
   def cobble
     self.condition = "new"
