35c14135e2ead308baa75a5207badc9482b15a74
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-08 21:16:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  
 
   def initialize(brand)
     @brand = brand
+    BRANDS = []
   end
 
   def brand=(brand)
