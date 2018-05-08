c37dca8bdc2be688c4451463c3b49b557d9dfe69
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-08 21:36:22 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,6 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-   if BRANDS.detect{|b| b.brand == brand} != true
-      BRANDS << brand
-    end
   end
 
   def brand=(brand)
@@ -17,6 +14,8 @@ class Shoe
   def brand
     @brand
   end
+
+  
 
   def cobble
     self.condition = "new"
